40b86a55e6e539c98416f38e1928f379c627a56c
bekzod003 <begiobm@gmail.com>
2021-10-20 17:16:59 +0500

    server files written

---
+++ pkg/server/grpc_contacts.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"grpc_task/pkg/proto"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+const connStr = "user=postgres password=7900 dbname=grpc_tasks sslmode=disable"
+
+var cDb, _ = sql.Open("postgres", connStr)
+
+type GRPCContactSrever struct {
+	proto.UnimplementedContacListServer
+}
+
+// Create and add are THE SAME
+func (grpc GRPCContactSrever) Create(c context.Context, cr *proto.ContactRequest) (*proto.ContactResponse, error) {
+	_, err := cDb.Exec(`insert into contacts (first_name, last_name, phone, email,)
+	values ( $1, $2, $3, $4);`, cr.C.GetFirstName(), cr.C.GetLastName(), cr.C.GetPhone(), cr.C.GetEmail())
+
+	return &proto.ContactResponse{C: cr.C}, err
+}
+
+func (grpc GRPCContactSrever) Add(c context.Context, cr *proto.ContactRequest) (*proto.ContactResponse, error) {
+	_, err := cDb.Exec(`insert into contacts (first_name, last_name, phone, email,)
+	values ( $1, $2, $3, $4);`, cr.C.GetFirstName(), cr.C.GetLastName(), cr.C.GetPhone(), cr.C.GetEmail())
+
+	return &proto.ContactResponse{C: cr.C}, err
+}
+
+func (grpc GRPCContactSrever) Update(c context.Context, cr *proto.ContactRequest) (*proto.ContactResponse, error) {
+	_, err := cDb.Exec(`update contacts 
+	set first_name = $1, last_name = $2, phone = $3, email = $4
+	where id = $5;`, cr.C.GetFirstName(), cr.C.GetLastName(), cr.C.GetPhone(), cr.C.GetEmail(), cr.C.GetId())
+
+	return &proto.ContactResponse{C: cr.C}, err
+}
+
+func (grpc GRPCContactSrever) Delete(c context.Context, cr *proto.ContactRequest) (*proto.ContactResponse, error) {
+	_, err := cDb.Exec(`delete from contacts where id = $1;`, cr.C.GetId())
+
+	return &proto.ContactResponse{C: cr.C}, err
+}
+
+func (grpc GRPCContactSrever) Get(c context.Context, crid *proto.ContactRequestID) (*proto.ContactResponse, error) {
+	row := cDb.QueryRow(`select * from contacts where id = $1;`, crid.GetId())
+
+	var tempCont proto.Contact
+	row.Scan(&tempCont.Id, &tempCont.FirstName, &tempCont.LastName, &tempCont.Phone, &tempCont.Email)
+
+	return &proto.ContactResponse{C: &tempCont}, nil
+}
+
+func (grpc GRPCContactSrever) GetAll(c context.Context, emp *emptypb.Empty) (*proto.ContactResponseSlice, error) {
+	rows, err := cDb.Query(`select * from contacts;`)
+
+	var conts = make([]*proto.Contact, 0)
+
+	for rows.Next() {
+		var tempCont proto.Contact
+		rows.Scan(&tempCont.Id, tempCont.FirstName, tempCont.LastName, &tempCont.Phone, &tempCont.Email)
+		conts = append(conts, &tempCont)
+	}
+	return &proto.ContactResponseSlice{C: conts}, err
+}
---
+++ pkg/server/grpc_tasks.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"grpc_task/pkg/proto"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+//const connStr = "user=postgres password=7900 dbname=grpc_tasks sslmode=disable"
+
+// opening taskDB
+var tDb, _ = sql.Open("postgres", connStr)
+
+type GRPCTasksSrever struct {
+	proto.UnimplementedTaskListServer
+}
+
+// Create and Add are THE SAME
+func (grpc GRPCTasksSrever) Create(c context.Context, tr *proto.TaskRequest) (*proto.TaskResponse, error) {
+	_, err := tDb.Exec(`insert into tasks (name, status, priority, created_at, created_by, due_date)
+	values ($1, $2, $3, $4, $5, $6);`, tr.T.GetName(), tr.T.GetStatus(), tr.T.GetPriority(),
+		tr.T.GetCreatedAt(), tr.T.GetCreatedBy(), tr.T.GetDueDate())
+
+	return &proto.TaskResponse{T: tr.T}, err
+}
+
+func (grpc GRPCTasksSrever) Add(c context.Context, tr *proto.TaskRequest) (*proto.TaskResponse, error) {
+	_, err := tDb.Exec(`insert into tasks (name, status, priority, created_at, created_by, due_date)
+	values ($1, $2, $3, $4, $5, $6);`, tr.T.GetName(), tr.T.GetStatus(), tr.T.GetPriority(),
+		tr.T.GetCreatedAt(), tr.T.GetCreatedBy(), tr.T.GetDueDate())
+
+	return &proto.TaskResponse{T: tr.T}, err
+}
+
+func (grpc GRPCTasksSrever) Update(c context.Context, tr *proto.TaskRequest) (*proto.TaskResponse, error) {
+	_, err := tDb.Exec(`update tasks
+	set name = $1, status = $2, priority = $3, due_date = $4
+	where id = $5;`, tr.T.GetName(), tr.T.GetStatus(), tr.T.GetPriority(), tr.T.GetDueDate(), tr.T.GetId())
+
+	return &proto.TaskResponse{T: tr.T}, err
+}
+
+func (grpc GRPCTasksSrever) Delete(c context.Context, tr *proto.TaskRequest) (*proto.TaskResponse, error) {
+	_, err := tDb.Exec(`delete from tasks where id = $1`, tr.T.GetId())
+
+	return &proto.TaskResponse{T: tr.T}, err
+}
+
+func (grpc GRPCTasksSrever) Get(c context.Context, trid *proto.TaskRequestID) (*proto.TaskResponse, error) {
+	row := tDb.QueryRow(`select * from tasks where id = $1`, trid.GetId())
+
+	var result proto.Task
+
+	row.Scan(&result.Id, &result.Name, &result.Status, &result.Priority, &result.CreatedAt,
+		&result.CreatedBy, &result.DueDate)
+
+	return &proto.TaskResponse{T: &result}, nil
+}
+
+func (grpc GRPCTasksSrever) GetAll(c context.Context, emp *emptypb.Empty) (*proto.TaskResponseSlice, error) {
+	rows, err := tDb.Query(`select * from tasks`)
+
+	var result = make([]*proto.Task, 0)
+	for rows.Next() {
+		var tempTask proto.Task
+		rows.Scan(&tempTask.Id, &tempTask.Name, &tempTask.Status, &tempTask.Priority,
+			&tempTask.CreatedAt, &tempTask.CreatedBy, &tempTask.CreatedBy)
+		result = append(result, &tempTask)
+	}
+
+	return &proto.TaskResponseSlice{T: result}, err
+}
